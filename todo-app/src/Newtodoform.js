6469ffb10fdde28b2a76f62e4f21e85d4ea132fc
jdvachhani95 <jaydeepvachhani22@gmail.com>
2019-10-17 14:41:47 -0400

    add methods and form

--- todo-app/src/Newtodoform.js
+++ todo-app/src/Newtodoform.js
@@ -3,16 +3,33 @@ import React, { Component } from 'react'
 class Newtodoform extends Component{
     constructor(props){
         super(props);
-        this.state={};
+        this.state={ task:''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     };
+
+    handleChange(evt){
+        this.setState({[evt.target.name] : evt.target.value});
+    }
+
+    handleSubmit(evt){
+        evt.preventDefault();
+        this.props.createTodo({...this.state, id:uuid(), completed:false});
+        this.setState({task:''});
+    }
 
     render(){
         return(
             <div className='Newtodoform'>
                 <h1>hi there!</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlfor='task'>New Todo</label>
+                    <input type='text' id='task' name='task' value={this.state.task} onChange={this.handleChange} placeholder='New Todo'/>        
+                    <button>Add Todo</button>
+                </form>
             </div>
         );
-    };
+    }
 }
 
 export default Newtodoform;
