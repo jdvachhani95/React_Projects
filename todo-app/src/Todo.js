33d77b0142141880906aa14219b1b4ad926a165d
jdvachhani95 <jaydeepvachhani22@gmail.com>
2019-10-17 19:09:35 -0400

    add edit functionality

--- todo-app/src/Todo.js
+++ todo-app/src/Todo.js
@@ -3,18 +3,67 @@ import React, { Component } from 'react'
 class Todo extends Component{
     constructor(props){
         super(props);
-        this.handleRemove = this.handleRemove.bind(this); 
+        this.state = {
+            isEditing: false,
+            task: this.props.task
+        };
+        this.handleRemove = this.handleRemove.bind(this);
+        this.toggleForm = this.toggleForm.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleUpdate = this.handleUpdate.bind(this);
+        this.handleToggle = this.handleToggle.bind(this);
     }
 
     handleRemove(){
         this.props.removeTodo(this.props.id);
     }
+
+    toggleForm() {
+        this.setState({ isEditing: !this.state.isEditing });
+    }
+    
+    handleUpdate(evt) {
+        evt.preventDefault();
+        //take new task data and pass up to parent
+        this.props.updateTodo(this.props.id, this.state.task);
+        this.setState({ isEditing: false });
+    }
+
+    handleChange(evt) {
+        this.setState({
+          [evt.target.name]: evt.target.value
+        });
+    }
+    handleToggle(evt) {
+        this.props.toggleTodo(this.props.id);
+    }
+
     render(){
+        let result;
+        if (this.state.isEditing) {
+            result = (
+                <form className='Todo-edit-form' onSubmit={this.handleUpdate}>
+                    <input
+                    type='text'
+                    value={this.state.task}
+                    name='task'
+                    onChange={this.handleChange}
+                    />
+                    <button>Save</button>
+                </form>
+            );
+        } 
+        else {
+            result = (
+                <li className='Todo-task' onClick={this.handleToggle}>
+                    {this.props.task}
+                </li>
+            );
+        }
         return(
             <div className='Todo'>
                 <button>edit</button>
                 <button onClick={this.handleRemove}>X</button>
-                <li>{this.props.task}</li>
             </div>
         );
     }
