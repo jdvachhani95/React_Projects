0e4b1f4bd360d8c9b83af2608de18ce294e3f369
jdvachhani95 <jaydeepvachhani22@gmail.com>
2019-10-17 19:23:43 -0400

    update Todolist component

--- todo-app/src/Todolist.js
+++ todo-app/src/Todolist.js
@@ -8,6 +8,8 @@ class Todolist extends Component{
         this.state={ todos:[]};
         this.create = this.create.bind(this);
         this.remove = this.remove.bind(this);
+        this.update = this.update.bind(this);
+        this.toggleCompletion = this.toggleCompletion.bind(this);
     };
 
     create(newTodo){
@@ -22,13 +24,43 @@ class Todolist extends Component{
         });
     }
 
+    update(id, updatedTask) {
+        const updatedTodos = this.state.todos.map(todo => {
+          if (todo.id === id) {
+            return { ...todo, task: updatedTask };
+          }
+          return todo;
+        });
+        this.setState({ todos: updatedTodos });
+    }
+
+    toggleCompletion(id) {
+        const updatedTodos = this.state.todos.map(todo => {
+          if (todo.id === id) {
+            return { ...todo, completed: !todo.completed };
+          }
+          return todo;
+        });
+        this.setState({ todos: updatedTodos });
+    }
+
     render(){
         const todos = this.state.todos.map(todo => {
-            return <Todo key={todo.id} task={todo.task}/>
+            return(
+                <Todo
+                    key={todo.id}
+                    id={todo.id}
+                    task={todo.task}
+                    completed={todo.completed}
+                    removeTodo={this.remove}
+                    updateTodo={this.update}
+                    toggleTodo={this.toggleCompletion}
+                />
+            );
         });
         return(
             <div className='Todolist'>
-                <Newtodoform id= {this.id} createTodo = {this.create} removeTodo = {this.remove}/> 
+                <Newtodoform createTodo = {this.create} /> 
                 <h1>Todo List!</h1>
                 <ul>{todos}</ul>
             </div>
